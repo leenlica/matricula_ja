53dec10efd3bb7fbee270e1c6afa18d03a25ebd0
leenlica <felix.s.hellen@gmail.com>
2024-09-17 22:09:23 -0300

    updated

--- matricula_back_front/public/alertas.js
+++ matricula_back_front/public/alertas.js
@@ -16,35 +16,30 @@ async function emitirAlerta(titulo, descricao, usuarios) {
     }
 }
 
-// Manipulador de eventos para o envio do formulário
 document.getElementById('alertForm').addEventListener('submit', async function (event) {
-    event.preventDefault(); // Evita o envio padrão do formulário
+    event.preventDefault(); 
 
-    // Captura os dados do formulário
     const titulo = document.getElementById('title').value;
     const descricao = document.getElementById('description').value;
 
-    // Verifica qual opção de destinatário foi selecionada
     const destinatarioAluno = document.getElementById('destinatarioAluno').checked;
     let usuarios = [];
 
     if (destinatarioAluno) {
         const idAluno = document.getElementById('idAluno').value;
         if (idAluno) {
-            usuarios = [idAluno]; // Se for um aluno específico, adiciona o ID do aluno
+            usuarios = [idAluno];
         } else {
             alert('Por favor, insira o ID do aluno.');
             return;
         }
     }
 
-    // Verifica se todos os campos necessários foram preenchidos
     if (!titulo || !descricao) {
         alert('Título e descrição são obrigatórios.');
         return;
     }
 
-    // Chama a função de emitir alerta com os dados do formulário
     try {
         await emitirAlerta(titulo, descricao, usuarios);
     } catch (error) {
