224895b1dd63c6ee7d49fd29acae6e6a16c470dc
JoaoVictorDevMeta <joaovictorgouveiar@gmail.com>
2024-09-25 22:18:38 -0300

    integração realizada

--- matricula_back_front/public/dashboard.js
+++ matricula_back_front/public/dashboard.js
@@ -2,157 +2,187 @@ document.getElementById('menu-toggle').addEventListener('click', function () {
     document.querySelector('.navbar-icons').classList.toggle('show');
 });
 
+async function fetchDataAndRenderCharts() {
+    try {
+        const responseVagas = await fetch('http://localhost:3000/vagas/1/2024');
+        const responseMatriculas = await fetch('http://localhost:3000/matriculas/1/2024');
+        const dataVagas = await responseVagas.json();
+        const dataMatriculas = await responseMatriculas.json();
 
-// valores fixos
-const vagasDisponiveis = 200;
-const preMatriculasConfirmadas = 150;
-const matriculasConfirmadas = 100;
+        var vagasDisponiveis = dataVagas[0].qtde_vagas_disponiveis;
+        var vagasOfertadas = dataVagas[0].qtde_vagas_ofertadas;
+        var preMatriculasConfirmadas = dataMatriculas.length;
+        var matriculasConfirmadas = dataMatriculas.filter(matricula => matricula.datahora_matricula !== null).length;
 
+        document.getElementById('vagas-disponiveis').innerText = vagasDisponiveis;
+        document.getElementById('pre-matriculas').innerText = preMatriculasConfirmadas;
+        document.getElementById('matriculas').innerText = matriculasConfirmadas;
 
-document.getElementById('vagas-disponiveis').innerText = vagasDisponiveis;
-document.getElementById('pre-matriculas').innerText = preMatriculasConfirmadas;
-document.getElementById('matriculas').innerText = matriculasConfirmadas;
-
-//gráfico de barras para as matrículas
-const ctx = document.getElementById('matriculasChart').getContext('2d');
-const matriculasChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Vagas Disponíveis', 'Pré-Matrículas Confirmadas', 'Matrículas Confirmadas'],
-        datasets: [{
-            data: [vagasDisponiveis, preMatriculasConfirmadas, matriculasConfirmadas],
-            backgroundColor: [
-                'rgba(82, 100, 225, 0.6)',
-                'rgba(211, 27, 230, 0.6)',
-                'rgba(17, 210, 65, 0.6)'
-            ],
-            borderColor: [
-                'rgba(82, 100, 225, 0.6)',
-                'rgba(211, 27, 230, 0.6)',
-                'rgba(17, 210, 65, 0.6)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        },
-        responsive: true,
-        plugins: {
-            legend: {
-                display: false 
+        // gráfico de barras para as matrículas
+        const ctx = document.getElementById('matriculasChart').getContext('2d');
+        const matriculasChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Vagas Disponíveis', 'Pré-Matrículas Confirmadas', 'Matrículas Confirmadas'],
+                datasets: [{
+                    data: [vagasDisponiveis, preMatriculasConfirmadas, matriculasConfirmadas],
+                    backgroundColor: [
+                        'rgba(82, 100, 225, 0.6)',
+                        'rgba(211, 27, 230, 0.6)',
+                        'rgba(17, 210, 65, 0.6)'
+                    ],
+                    borderColor: [
+                        'rgba(82, 100, 225, 0.6)',
+                        'rgba(211, 27, 230, 0.6)',
+                        'rgba(17, 210, 65, 0.6)'
+                    ],
+                    borderWidth: 1
+                }]
             },
-            tooltip: {
-                callbacks: {
-                    label: function (tooltipItem) {
-                        const value = tooltipItem.raw;
-                        const label = tooltipItem.label;
-                        return `${label}: ${value}`;
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                },
+                responsive: true,
+                plugins: {
+                    legend: {
+                        display: false 
+                    },
+                    tooltip: {
+                        callbacks: {
+                            label: function (tooltipItem) {
+                                const value = tooltipItem.raw;
+                                const label = tooltipItem.label;
+                                return `${label}: ${value}`;
+                            }
+                        }
                     }
                 }
             }
+        });
+
+        var vagasDisponiveisData = [vagasOfertadas]
+        var preMatriculasData = [0]
+        var matriculasData = [0]
+
+        for(let i = 0; i < 3; i++) { // loop fixo para os três meses do ano
+            let vagas = dataMatriculas?.filter(item => {
+                let date = new Date(item.datahora_prematricula);
+                return date.getMonth() === i; 
+            }).length;
+            vagasDisponiveisData.push(vagasDisponiveisData[i] - vagas); // esses números sempre vão ser opostos
+            preMatriculasData.push(preMatriculasData[i] + vagas); // esses números sempre vão ser opostos
+
+            let matriculas = dataMatriculas?.filter(item => {
+                let date = new Date(item.datahora_matricula);
+                return date.getMonth() === i; 
+            }).length;
+            matriculasData.push(matriculasData[i] + matriculas);
         }
-    }
-});
 
-// Valores simulados para ao longo dos meses
-const vagasDisponiveisData = [vagasDisponiveis + 190, vagasDisponiveis +30, vagasDisponiveis +15, vagasDisponiveis]; 
-const preMatriculasData = [preMatriculasConfirmadas - 40, preMatriculasConfirmadas - 20, preMatriculasConfirmadas - 10, preMatriculasConfirmadas]; 
-const matriculasData = [matriculasConfirmadas - 20, matriculasConfirmadas - 10, matriculasConfirmadas - 5, matriculasConfirmadas]; 
+        // Valores simulados para ao longo dos meses
+        //const vagasDisponiveisData = [vagasOfertadas, vagasOfertadas - vagasJan, vagasOfertadas, vagasDisponiveis]; 
+        //const preMatriculasData = [preMatriculasConfirmadas, preMatriculasConfirmadas, preMatriculasConfirmadas, preMatriculasConfirmadas]; 
+        //const matriculasData = [matriculasConfirmadas, matriculasConfirmadas , matriculasConfirmadas , matriculasConfirmadas]; 
 
-// gráfico de linha para matrículas ao longo dos meses
-const ctl = document.getElementById('newLineChart').getContext('2d');
-const newLineChart = new Chart(ctl, {
-    type: 'line',
-    data: {
-        labels: ['Dezembro', 'Janeiro', 'Fevereiro', 'Março'],
-        datasets: [
-            {
-                label: 'Vagas Disponíveis',
-                data: vagasDisponiveisData,
-                fill: false,
-                borderColor: 'rgba(82, 100, 225, 1)',
-                tension: 0.1
+        // gráfico de linha para matrículas ao longo dos meses
+        const ctl = document.getElementById('newLineChart').getContext('2d');
+        const newLineChart = new Chart(ctl, {
+            type: 'line',
+            data: {
+                labels: ['Dezembro', 'Janeiro', 'Fevereiro', 'Março'],
+                datasets: [
+                    {
+                        label: 'Vagas Disponíveis',
+                        data: vagasDisponiveisData,
+                        fill: false,
+                        borderColor: 'rgba(82, 100, 225, 1)',
+                        tension: 0.1
+                    },
+                    {
+                        label: 'Pré-Matrículas Confirmadas',
+                        data: preMatriculasData,
+                        fill: false,
+                        borderColor: 'rgba(211, 27, 230, 1)',
+                        tension: 0.1
+                    },
+                    {
+                        label: 'Matrículas Confirmadas',
+                        data: matriculasData,
+                        fill: false,
+                        borderColor: 'rgba(17, 210, 65, 1)',
+                        tension: 0.1
+                    }
+                ]
             },
-            {
-                label: 'Pré-Matrículas Confirmadas',
-                data: preMatriculasData,
-                fill: false,
-                borderColor: 'rgba(211, 27, 230, 1)',
-                tension: 0.1
-            },
-            {
-                label: 'Matrículas Confirmadas',
-                data: matriculasData,
-                fill: false,
-                borderColor: 'rgba(17, 210, 65, 1)',
-                tension: 0.1
-            }
-        ]
-    },
-    options: {
-        responsive: true,
-        plugins: {
-            legend: {
-                display: true,
-                position: 'top'
-            },
-            tooltip: {
-                callbacks: {
-                    label: function (tooltipItem) {
-                        const value = tooltipItem.raw;
-                        return `${tooltipItem.dataset.label}: ${value}`;
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        display: true,
+                        position: 'top'
+                    },
+                    tooltip: {
+                        callbacks: {
+                            label: function (tooltipItem) {
+                                const value = tooltipItem.raw;
+                                return `${tooltipItem.dataset.label}: ${value}`;
+                            }
+                        }
+                    }
+                },
+                scales: {
+                    y: {
+                        beginAtZero: true
                     }
                 }
             }
-        },
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+        });
 
-// gráfico em pizza
-const cts = document.getElementById('totalsPieChart').getContext('2d');
-const totalsPieChart = new Chart(cts, {
-    type: 'pie',
-    data: {
-        labels: ['Vagas Disponíveis', 'Pré-Matrículas Confirmadas', 'Matrículas Confirmadas'],
-        datasets: [{
-            data: [vagasDisponiveis, preMatriculasConfirmadas, matriculasConfirmadas],
-            backgroundColor: [
-                'rgba(82, 100, 225, 0.7)',
-                'rgba(211, 27, 230, 0.7)',
-                'rgba(17, 210, 65, 0.7)'
-            ],
-            borderColor: [
-                'rgba(120, 120, 120, 0.5)',
-                'rgba(120, 120, 120, 0.5)',
-                'rgba(120, 120, 120, 0.5)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        responsive: true,
-        plugins: {
-            legend: {
-                position: 'top',
+        // gráfico em pizza
+        const cts = document.getElementById('totalsPieChart').getContext('2d');
+        const totalsPieChart = new Chart(cts, {
+            type: 'pie',
+            data: {
+                labels: ['Vagas Disponíveis', 'Pré-Matrículas Confirmadas', 'Matrículas Confirmadas'],
+                datasets: [{
+                    data: [vagasDisponiveis, preMatriculasConfirmadas, matriculasConfirmadas],
+                    backgroundColor: [
+                        'rgba(82, 100, 225, 0.7)',
+                        'rgba(211, 27, 230, 0.7)',
+                        'rgba(17, 210, 65, 0.7)'
+                    ],
+                    borderColor: [
+                        'rgba(120, 120, 120, 0.5)',
+                        'rgba(120, 120, 120, 0.5)',
+                        'rgba(120, 120, 120, 0.5)'
+                    ],
+                    borderWidth: 1
+                }]
             },
-            tooltip: {
-                callbacks: {
-                    label: function (tooltipItem) {
-                        const value = tooltipItem.raw;
-                        const label = tooltipItem.label;
-                        return `${label}: ${value}`;
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    tooltip: {
+                        callbacks: {
+                            label: function (tooltipItem) {
+                                const value = tooltipItem.raw;
+                                const label = tooltipItem.label;
+                                return `${label}: ${value}`;
+                            }
+                        }
                     }
                 }
             }
-        }
+        });
+    } catch (error) {
+        console.error('Error fetching data:', error);
     }
-});
+}
+
+document.addEventListener('DOMContentLoaded', fetchDataAndRenderCharts);
